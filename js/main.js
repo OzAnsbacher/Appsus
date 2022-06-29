03164b3293cac54f2c7c90c7e292ee2e26ea59b3
Oz Ansbacher <ozansbacher@gmail.com>
2022-06-29 11:18:52 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const { createApp } = Vue
+
+const options = {
+    template: `
+  
+      `,
+    components: {
+
+    }
+  
+  }
+
+
+  const app = createApp(options)
+app.use(router)
+app.mount('#app')
+
+
