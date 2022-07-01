a8eb21addc509497c84776215afac8defe76a3df
Oz Ansbacher <ozansbacher@gmail.com>
2022-07-01 12:47:21 +0300

    style compose

--- js/apps/mail/cmps/mail-compose.cmp.js
+++ js/apps/mail/cmps/mail-compose.cmp.js
@@ -2,15 +2,24 @@ import { utilService } from "../../../services/util.service.js";
 
 export default {
     template: `
- <section>
- <router-link to="/mail"><button>Inbox</button></router-link>
-    <router-link to="/mail/compose"><button>Compose</button></router-link>
+ <section class="compose-conteiner" >
+ <div class="mail-list-conteiner flex">
+    <div class="nav-mail compose-nav flex">
+        <router-link to="/mail"><img class="btn-nav-mail" src="../../../icons/inboxmail.png" alt=""></router-link>
+        <router-link to="/mail/compose"><img class="btn-nav-mail" src="../../../icons/newmail.png" alt=""></router-link>
+        <router-link to="/mail/send"><img class="btn-nav-mail" src="../../../icons/sendmail.png" alt=""></router-link>
+    </div>
+ <!-- <router-link to="/mail"><button>Inbox</button></router-link>
+    <router-link to="/mail/compose"><button>compose</button></router-link>
     <router-link to="/mail/send"><button>Sent</button></router-link>
- <p>{{sentMail}}</p>
-     <input type="email" v-model="sentMail.to" id="addressEmail" placeholder="to:" autofocus>
-     <input type="text" v-model="sentMail.subject" id="subjectEmail" placeholder="subject:">
-     <textarea name="" v-model="sentMail.body" id="bodyTxtEmail" cols="30" rows="10"></textarea>
-     <button @click="sentingMail">Send</button>
+ <p>{{sentMail}}</p> -->
+ <div class="compose-main">
+     <input type="email" class="compose-adrress" v-model="sentMail.to" id="addressEmail" placeholder="to:" autofocus>
+     <button class="compose-btn" @click="sentingMail">Send</button>
+     <input type="text" class="compose-subject" v-model="sentMail.subject" id="subjectEmail" placeholder="subject:">
+     <textarea name="" class="compose-txt" v-model="sentMail.body" id="bodyTxtEmail" cols="30" rows="10"></textarea>
+     </div>
+     </div>
   </section>
 `,
     data() {
